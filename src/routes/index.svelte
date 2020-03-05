e6a55892384c7b73b96c82439f5610fc92f0fe35
Dennis Ostendorf <dennisostendorf@web.de>
2020-03-05 12:23:34 +0100

    Change heading for automation testing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,6 +36,6 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Test change, test heading</h1>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
