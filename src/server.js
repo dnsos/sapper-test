4a75e3caa5862ac40bc81e312121f30a00fc398c
Dennis Ostendorf <dennisostendorf@web.de>
2020-03-06 10:05:51 +0100

    Update correct subdirectory path

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-		'sapper-test',
+		'/sapper-test',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
